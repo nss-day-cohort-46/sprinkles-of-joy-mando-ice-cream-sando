40883ee034b9de7dc3ca469ea02ba2aeae2a8db9
Dan Ross <daniellross@gmail.com>
2021-02-19 10:18:41 -0600

    delete button only renders if the order status is ready to pick up

--- scripts/orders/Order.js
+++ scripts/orders/Order.js
@@ -6,6 +6,16 @@ export const Order = (customerOrder, productArray) => {
     totalPrice += product.price                             //adding the price of each individual item
     return `<li>${product.name}</li>`                       //putting the individual product name into list elements
   }).join("")
+  
+  debugger
+
+  const deleteButton = () => {
+    if (customerOrder.status.id === 1){
+      return `<button id="deleteOrder--${customerOrder.id}">Delete</button>`
+    } else {
+      return ``
+    }
+  }
 
   return `
     <div class="order">
@@ -14,5 +24,6 @@ export const Order = (customerOrder, productArray) => {
       <ul>${productArrayHTMLrep}</ul>
     <p>${totalPrice}</p>
     </div>
+    ${deleteButton()}
   `
 }
