e4bbdfc9be20b2b5202c743ff57d35f2fb6e1518
jakefroeb <jake.herman.froeb@gmail.com>
2021-02-18 18:23:35 -0600

    got the thing working with tons of annotations as well manipulating through multiple many to many relationships required a lot of work, i probably have some redundant code in there but it works for now

--- scripts/orders/Order.js
+++ scripts/orders/Order.js
@@ -1,8 +1,19 @@
-export const Order = (customerOrder) => {
+
+
+export const Order = (customerOrder, productArray) => {
+  let totalPrice = 0
+  console.log(productArray)
+  let productArrayHTMLrep = productArray.map(product => {
+    totalPrice += product.price                             //adding the price of each individual item
+    return `<li>${product.name}</li>`                       //putting the individual product name into list elements
+  }).join("")
+
   return `
     <div class="order">
       <p>${new Date(customerOrder.timestamp).toLocaleString('en-US')}</p>
       <p>${customerOrder.status.label}</p>
+      <ul>${productArrayHTMLrep}</ul>
+    <p>${totalPrice}</p>
     </div>
   `
 }
