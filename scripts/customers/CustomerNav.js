402fdfb51ffe38fe65c1af4cee0c85206f63e7a8
jakefroeb <jake.herman.froeb@gmail.com>
2021-02-19 13:46:00 -0600

    logout working added a key value to authhelper that has a key of logout and a value that is a function that clears session storage

--- scripts/customers/CustomerNav.js
+++ scripts/customers/CustomerNav.js
@@ -1,17 +1,29 @@
 import { authHelper } from "../auth/authHelper.js"
 import { getCustomer } from "../customers/CustomerProvider.js"
+import { LoginForm } from "./LoginForm.js"
 
 const eventHub = document.querySelector("#container")
 const userNav = document.querySelector(".userNav")
+const logOutButtonTarget = document.querySelector(".navigation")
 
 export const CustomerNav = () => {
   if (authHelper.isUserLoggedIn()) {
     getCustomer(authHelper.getCurrentUserId())
       .then(userObject => {
         render(userObject)
+        logOutButtonTarget.innerHTML += '<li><a href="#" id="logOutLink">Log Out</a></li>'
       })
   }
 }
+eventHub.addEventListener("click", e =>{
+    if(e.target.id === "logOutLink"){
+      userNav.innerHTML = ""
+      authHelper.logOut()
+      logOutButtonTarget.innerHTML = ""
+      LoginForm()
+    }
+})
+
 
 const render = (customer) => {
   userNav.innerHTML = `
